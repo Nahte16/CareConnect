5da90e63d5fda48ba68b1562232cc46ff17f9253
jehoshuase <mjselen@tip.edu.ph>
2023-05-08 00:03:38 +0800

    partial find doctor

---
+++ app/src/main/java/com/example/careconnect1/FindDoctorActivity.java
@@ -0,0 +1,34 @@
+package com.example.careconnect1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class FindDoctorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_doctor);
+
+        CardView exit = findViewById(R.id.cardFDBack);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FindDoctorActivity.this,HomeActivity.class));
+            }
+        });
+        CardView familyPhysician = findViewById(R.id.cardFDFamilyPhysician);
+        familyPhysician.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it= new Intent(FindDoctorActivity.this,DoctorDetailsActivity.class);
+                it.putExtra("title","Family Physicians");
+                startActivity(it);
+            }
+        });
+    }
+}
