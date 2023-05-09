e2e5a6c68c5b49b4537b8730b7e13114e40992d2
Scott Mamhut <Mamhutscott23@gmail.com>
2023-05-09 14:03:05 +0800

    Added some missing class to HomeActivity but not the class literally just the onclickListener.

--- app/src/main/java/com/example/careconnect1/HomeActivity.java
+++ app/src/main/java/com/example/careconnect1/HomeActivity.java
@@ -38,5 +38,29 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(new Intent(HomeActivity.this,FindDoctorActivity.class));
             }
         });
+
+        CardView orderDetails = findViewById(R.id.cardOrderDetails);
+        orderDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,OrderDetailsActivity.class));
+            }
+        });
+
+        CardView buyMedicine = findViewById(R.id.cardBuyMedicine);
+        buyMedicine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,BuyMedicineActivity.class));
+            }
+        });
+
+        CardView health = findViewById(R.id.cardHealthDoctor);
+        health.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,HealthArticlesActivity.class));
+            }
+        });
     }
 }
