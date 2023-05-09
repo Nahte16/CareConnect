af87a32320ee5d456b8fd1e24f15e86474fc6f81
jehoshuase <mjselen@tip.edu.ph>
2023-05-09 17:27:27 +0800

    done Book Appointment but else condition in button book not yet working

--- app/src/main/java/com/example/careconnect1/DoctorDetailsActivity.java
+++ app/src/main/java/com/example/careconnect1/DoctorDetailsActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -12,48 +13,47 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class DoctorDetailsActivity extends AppCompatActivity {
     private String[][] doctor_details1 =
             {
-                    {"Doctor Name : Dr. Andrea Carla Adajar", "Hospital Address: Online Clinic using NowServing app ","Experience: 2 years", "150"},
-                    {"Doctor Name : Dr. Sheila Marie Casasola", "Hospital Address: Online Clinic using NowServing app ","Experience: 1 year", "150"},
-                    {"Doctor Name : Dr. Michelle Joan Tagle-Ortega", "Hospital Address: Online Clinic using NowServing app ","Experience: 5 years", "-"},
-                    {"Doctor Name : Dr. Eduardo Jr Fillaro", "Hospital Address: Online Clinic using NowServing app ","Experience: 9 years", "200"},
-                    {"Doctor Name : Dr. Polli Kathlene Paulo", "Hospital Address: Online Clinic using NowServing app ","Experience: 4 years", "270"},
+                    {"Dr. Andrea Carla Adajar", "Online Clinic  ","2 years", "150"},
+                    {"Dr. Sheila Marie Casasola", "Online Clinic  ","1 year", "150"},
+                    {"Dr. Michelle Joan Tagle-Ortega", "Online Clinic ","5 years", "-"},
+                    {"Dr. Eduardo Jr Fillaro", "Online Clinic ","9 years", "200"},
+                    {"Dr. Polli Kathlene Paulo", "Online Clinic ","4 years", "270"},
             };
     private String[][] doctor_details2 =
             {
-                    {"Doctor Name : Dr. Lyka Erika Labsan", "Hospital Address: Online Clinic using NowServing app ","Experience: 3 years", "300"},
-                    {"Doctor Name : Dr. Benson Panaguiton", "Hospital Address: Online Clinic using NowServing app ","Experience: 5 years", "285"},
-                    {"Doctor Name : Dr. Francis Joshua Beloy", "Hospital Address: Online Clinic using NowServing app ","Experience: 2 years", "335"},
-                    {"Doctor Name : Dr. Anna Lorraine Bantugay", "Hospital Address: Online Clinic using NowServing app ","Experience: 8 years", "800"},
-                    {"Doctor Name : Dr. Reynaldo Cruz", "Hospital Address: Online Clinic using NowServing app ","Experience: 20 years", "880"},
+                    {"Dr. Lyka Erika Labsan", "Online Clinic ","3 years", "300"},
+                    {"Dr. Benson Panaguiton", "Online Clinic ","5 years", "285"},
+                    {"Dr. Francis Joshua Beloy", "Online Clinic ","2 years", "335"},
+                    {"Dr. Anna Lorraine Bantugay", "Online Clinic ","8 years", "800"},
+                    {"Dr. Reynaldo Cruz", "Online Clinic ","20 years", "880"},
             };
     private String[][] doctor_details3 =
             {
-                    {"Doctor Name : Dr. Jiren Mare Jamantoc", "Hospital Address: Online Clinic using NowServing app and St. Michael's Dental Care ","Experience: 8 years", "700"},
-                    {"Doctor Name : Dr. Rose Eden Tanoja", "Hospital Address: Online Clinic using NowServing app and SmartSmile Dental Clinic ","Experience: 19 years", "500"},
-                    {"Doctor Name : Dr. Catherine Han", "Hospital Address: Online Clinic using NowServing app and Catherine Andaya-Han Dental Clinic ","Experience: 11 years", "500"},
-                    {"Doctor Name : Dr. Hana Jeishel Flores", "Hospital Address: Online Clinic using NowServing app and Roxas Dental Clinic","Experience: 4 years","-"},
-                    {"Doctor Name : Dr. John Daniel Villegas", "Hospital Address: Online Clinic using NowServing app and Zahn Aesthetix Dental Clinic ","Experience: 6 years", "500"},
+                    {"Dr. Jiren Mare Jamantoc", "Online Clinic/St. Michael's Dental Care ","8 years", "700"},
+                    {"Dr. Doctor Name : Rose Eden Tanoja", "Online Clinic/SmartSmile Dental Clinic ","19 years", "500"},
+                    {"Dr. Catherine Han", "Online Clinic/Catherine Andaya-Han Dental Clinic ","11 years", "500"},
+                    {"Dr. Hana Jeishel Flores", "Online Clinic/Roxas Dental Clinic","4 years","-"},
+                    {"Dr. John Daniel Villegas", "Online Clinic/Zahn Aesthetix Dental Clinic ","6 years", "500"},
             };
     private String[][] doctor_details4 =
             {
-                    {"Doctor Name : Dr. Paul Julius Medina", "Hospital Address: Online Clinic using NowServing app and Mercy Spine Center ","Experience: 15 years","800"},
-                    {"Doctor Name : Dr. Alembert Lee-Ong", "Hospital Address: Online Clinic using NowServing app and MDH Rm 1107","Experience: 17 years", "-"},
-                    {"Doctor Name : Dr. Anthony Dofitas", "Hospital Address: Online Clinic using NowServing app and Manila Med Surgery Clinic ","Experience: 13 years", "-"},
-                    {"Doctor Name : Dr. Adrian Fernando", "Hospital Address: Online Clinic using NowServing app and 305 UERM ","Experience: 12 years", "1000"},
-                    {"Doctor Name : Dr. Leonardo Ona", "Hospital Address: Online Clinic using NowServing app and Leonardo Ona M.D.","Experience: 27 years", "1000"},
+                    {"Dr. Paul Julius Medina", "Online Clinic/Mercy Spine Center ","15 years","800"},
+                    {"Dr. Alembert Lee-Ong", "Online Clinic/MDH Rm 1107","17 years", "-"},
+                    {"Dr. Anthony Dofitas", "Online Clinic/Manila Med Surgery Clinic ","13 years", "-"},
+                    {"Dr. Doctor Name : Adrian Fernando", "Online Clinic/305 UERM ","12 years", "1000"},
+                    {"Dr. Leonardo Ona", "Online Clinic/ using NowServing app and Leonardo Ona M.D.","27 years", "1000"},
             };
     private String[][] doctor_details5 =
             {
-                    {"Doctor Name : Dr. Ma Minerva Boac", "Hospital Address: Online Clinic using NowServing app and South Imus Specialist Hospital ","Experience: 8 years", "700"},
-                    {"Doctor Name : Dr. Emiliano Jr Canonigo", "Hospital Address: Online Clinic using NowServing app and Rm 210 ASIAN HOSPITAL","Experience: 47 years", "-"},
-                    {"Doctor Name : Dr. Nannette Rey", "Hospital Address: Online Clinic using NowServing app and De La Salle University Medical Center ","Experience: 20 years", "-"},
-                    {"Doctor Name : Dr. Roi Joseph Mendoza", "Hospital Address: Online Clinic using NowServing app ","Experience: 8 years", "500"},
-                    {"Doctor Name : Dr. Roy Sasil Jr", "Hospital Address: One Heart @ Maria Reyna ","Experience: 7 years", "900"},
+                    {"Dr. Ma Minerva Boac", "Online Clinic/South Imus Specialist Hospital ","8 years", "700"},
+                    {"Dr. Emiliano Jr Canonigo", "Online Clinic/Rm 210 ASIAN HOSPITAL","47 years", "-"},
+                    {"Dr. Nannette Rey", "Online Clinic/De La Salle University Medical Center ","20 years", "-"},
+                    {"Dr. Doctor Name : Roi Joseph Mendoza", "Online Clinic ","8 years", "500"},
+                    {"Dr. DRoy Sasil Jr", "One Heart @ Maria Reyna ","7 years", "900"},
             };
     TextView tv;
     Button btn;
@@ -67,7 +67,7 @@ public class DoctorDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_doctor_details);
 
         tv = findViewById(R.id.textViewDDTitle);
-        btn = findViewById(R.id.buttonDDBack);
+        btn = findViewById(R.id.buttonAppBack);
         Intent it = getIntent();
         String title = it.getStringExtra("title");
         tv.setText(title);
@@ -93,9 +93,9 @@ public class DoctorDetailsActivity extends AppCompatActivity {
         list = new ArrayList();
         for (int i=0;i<doctor_details.length;i++){
              item = new HashMap<String,String>();
-             item.put("line1", doctor_details[i][0]);
-             item.put("line2", doctor_details[i][1]);
-             item.put("line3", doctor_details[i][2]);
+             item.put("line1", "Doctor Name: " + doctor_details[i][0]);
+             item.put("line2", "Hospital Address: " + doctor_details[i][1]);
+             item.put("line3", "Experience: " +doctor_details[i][2]);
              item.put("line4", "Cons Fees: ₱ "+doctor_details[i][3]);
              list.add(item);
         }
@@ -104,5 +104,17 @@ public class DoctorDetailsActivity extends AppCompatActivity {
                 );
         ListView lst = findViewById(R.id.ListViewDD);
         lst.setAdapter(sa);
+
+        lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent it = new Intent(DoctorDetailsActivity.this,BookAppointmentActivity.class);
+                it.putExtra("text1",title);
+                it.putExtra("text2",doctor_details[i][0]);
+                it.putExtra("text3",doctor_details[i][1]);
+                it.putExtra("text4",doctor_details[i][3]);
+                startActivity(it);
+            }
+        });
     }
 }
