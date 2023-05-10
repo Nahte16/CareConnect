e3651e6e79005720290bbed609be123e998ae7a0
Gian Carlo <mgcmilana@tip.edu.ph>
2023-05-11 00:36:30 +0800

    added add to cart function for LabTestDetailsActivity

--- app/src/main/java/com/example/careconnect1/LabTestDetailsActivity.java
+++ app/src/main/java/com/example/careconnect1/LabTestDetailsActivity.java
@@ -2,12 +2,15 @@ package com.example.careconnect1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LabTestDetailsActivity extends AppCompatActivity {
 
@@ -36,6 +39,26 @@ public class LabTestDetailsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LabTestDetailsActivity.this,LabTestActivity.class));
+            }
+        });
+
+        btnAddToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                String username = sharedpreferences.getString("username","").toString();
+                String product = tvPackageName.getText().toString();
+                float price = Float.parseFloat(intent.getStringExtra("Text3").toString());
+
+                Database db = new Database(getApplicationContext(),"healthcare",null,1);
+
+                if(db.checkCart(username,product)==1){
+                    Toast.makeText(getApplicationContext(),"Product Already Added",Toast.LENGTH_SHORT).show();
+                } else {
+                    db.addCart(username,product,price,"lab");
+                    Toast.makeText(getApplicationContext(),"Record Inserted to Cart",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LabTestDetailsActivity.this,LabTestActivity.class));
+                }
             }
         });
     }
